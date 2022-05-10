8f18daf97bd2e6cfbf9710cef8898ae5d18d1e92
Markitos <raxor708@hotmail.com>
2022-05-09 23:04:37 -0300

    cambiando mp directorio

--- Ejercicios/Multiprocessing/mp.py
+++ Ejercicios/Multiprocessing/mp.py
@@ -10,7 +10,10 @@ import multiprocessing as mp
 from codecs import encode #encode("str", "rot_13") 
 
 class Constants():
-    pass
+    STOP = "Para parar bucle escribir |stop|"
+    CHILD1_STOP = "Cerrando Hijo 1..."
+    CHILD2_STOP = "Cerrando Hijo 2..."
+    FATHER_STOP = "Cerrando Padre..." 
 
 class Main():
 
@@ -35,32 +38,32 @@ class Main():
         #Genero el pipe.
         child1_conn, child2_conn = mp.Pipe()
 
-        #Genero el segundo hijo
-        child2 = mp.Process(target = self.Child2, name = "Encripter", args = (child2_conn,))
         #Genero el primer hijo
-        child1 = mp.Process(target = self.Child1, name = "Terminal Reader", args = (child1_conn, child2.pid, fn))
-
-        #Inicio los hijos
-        child1.run()
-        child2.run()
+        child1 = mp.Process(target = self.Child1, name = "Terminal Reader", args = (child1_conn, fn, cons))
+        child1.start()
+        #Genero el segundo hijo
+        child2 = mp.Process(target = self.Child2, name = "Encripter", args = (child2_conn, child1.pid, cons))
+        child2.start()
 
         #Esperando a que los hijos se cierren para poder cerrarse.
         while True:
-            print(child1.is_alive())
-            print(child2.is_alive())
             if (not child1.is_alive() and not child2.is_alive()):
                 child1.join()
                 child2.join()
-                break;
+                print(cons.FATHER_STOP)
+                break
                 
 
 
-    def Child1(self, pipe, child2_PID, fn):
+    def Child1(self, pipe, fn, cons):
         """Lee la terminal y manda todo esos datos al pipe.
+
         args:
                 -pipe: Un extremo del pipe compartido entre los hijos.
-                -child2_PID: El número del pid del segundo hijo
-                -fn: Terminal principal."""
+                -fn: Terminal principal.
+                -cons: Constantes"""
+
+        print(cons.STOP)
 
         #Señal para obtener los datos del queue.
         signal.signal(signal.SIGUSR1, self.Child1_Print)
@@ -70,42 +73,60 @@ class Main():
 
         #Al escribir en la terminal lo mando al pipe.
         for str in sys.stdin:
-            if (str[:-1] == "stop"):
+
+            #Le quito el \n al texto
+            str = str[:-1]
+
+            if (str == "stop"):
                 #Envio una señal al child2 para que se cierre.
-                os.kill(child2_PID, signal.SIGUSR1)
+                pipe.send("stop")
+                print(cons.CHILD1_STOP)
                 exit(0)
 
             #Envio lo escrito al pipe.
             pipe.send(str)
 
-    def Child2(self, pipe, child1_PID):
+    def Child2(self, pipe, child1_PID, cons):
         """Realiza el encriptado rot13.
+
         args:
                 -pipe: El pipe del hijo.
-                -child1_PID: El número del pid del segundo hijo"""
-
-        #Genero una señal para cerrar el hijo 2 cuando se escriba "stop".
-        signal.signal(signal.SIGUSR1, self.Child2_Exit)
+                -child1_PID: El número del pid del segundo hijo
+                -cons: Constantes"""
 
         while True:
             #Recibo el texto del pipe.
             text = pipe.recv()
 
+            #Si el texto es stop que se cierre.
+            if(text == "stop"):
+                self.Child2_Exit(cons = cons)
+
             #Lo transformo a rot13
             text = encode(text, "rot_13")
 
             #Lo almaceno en queue.
-            q.put(text)
+            self.q.put(text)
 
             #Aviso por señal al hijo 1.
             os.kill(child1_PID, signal.SIGUSR1)
             
     def Child1_Print(self, s, f):
-        """Printar en pantalla el archivo encriptado."""
-        print(q.get())
+        """Printar en pantalla el archivo encriptado.
+        
+        args:
+                -s: Número de señal.
+                -f: FRAME. Puntero donde el proceso debe volver en stack al finalizar esta función."""
 
-    def Child2_Exit(self, s, f):
-        """Cerrar el programa hijo 2."""
+        print(self.q.get())
+
+    def Child2_Exit(self, cons):
+        """Cerrar el programa hijo 2.
+        
+        args:
+                -cons: Constantes"""
+            
+        print(cons.CHILD2_STOP)
         exit(0)
 
 #Arrancar ejemplo
